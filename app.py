2f9df4c78602a967d90143b3d280b5eeaa594f42
sabyun <hoone0802@gmail.com>
2025-08-03 19:16:11 +0900

    Feat: add debugMod
    디버그 추가 컴퓨터 호스트네임 표시

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
-
+import socket
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    if app.debug:
+        hostname = '컴퓨터(인스턴스)'+socket.gethostname()
+    else:
+        hostname = ''
+    return render_template('index.html', computername=hostname)
 
 @app.route('/image')
 def render_example():
@@ -15,4 +19,4 @@ def render_menu():
     return render_template('menu.html', page_type='menu')
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=80)
+    app.run("0.0.0.0", port=80,debug=True)
