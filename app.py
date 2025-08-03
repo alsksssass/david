65d64581e48ddbf13c6d8cc7c6deb19ead9803a3
sabyun <hoone0802@gmail.com>
2025-08-03 14:50:37 +0900

    Feat: 메뉴화면 추가 메뉴 세개 추가하고 렌터페이지 따로 만듬 동적으로 작동Resolves: #1

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ def hello_world():
 
 @app.route('/image')
 def render_example():
-    return render_template('index.html')
+    return render_template('index.html', page_type='image')
+
+@app.route('/menu')
+def render_menu():
+    return render_template('menu.html', page_type='menu')
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port=80)
